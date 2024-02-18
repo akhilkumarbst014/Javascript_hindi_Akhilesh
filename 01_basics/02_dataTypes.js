b9873f3aaff32d4b192d3a3329485821946663bc
akhilkumarbst014 <131418856+akhilkumarbst014@users.noreply.github.com>
2024-02-18 09:24:21 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";  // treat all JS code as newer version
+// alert(3+3) // we are using nodejs, not browser
+
+
+console.log(3+3); console.log("Akhil") //code readibility should be high
+/*OR*/
+console.log(3+3)
+console.log("Akhil")
+let name = "Akku"
+let age = 18
+let isLoggedIn = false
+
+/*                    Primitive datatype                       */
+// number => 2 to power 53
+// bigint // for large number
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value abhi defined nahi hai
+// symbol => unique ki baat karenge jab // it is mast used in react
+
+// object
+
+console.log(typeof "Akhil")
+console.log(typeof age)
+console.log(typeof null)// Important for interview // output=> object
+console.log(typeof undefined) // output=> undefined
