b5fa321010ce46bfcba5dc8b4737dc87da2eae38
akhilkumarbst014 <131418856+akhilkumarbst014@users.noreply.github.com>
2024-02-18 09:53:28 +0000

    for each reduce

---
+++ 05_iterations/06_for_each_reduce.js
@@ -0,0 +1,35 @@
+const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce(function (acc, currval) { // acc = "Accumulator" ek khali space hai jisme hame ,
+                                                           // kare jo 0 liha hai wo fist time accumulator me store hoti hai
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0)
+
+const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+
+console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
