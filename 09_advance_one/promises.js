cb287b7ff2bc04230460af4a4444ea5f6775976e
akhilkumarbst014 <131418856+akhilkumarbst014@users.noreply.github.com>
2024-02-19 04:51:43 +0000

    promises

---
+++ 09_advance_one/promises.js
@@ -0,0 +1,58 @@
+/* Creation of Promises */
+const promisesone = new Promise(function (resolve, reject) {
+    // Do an Async task
+    // DataBase calls, cryptography, network call
+    setTimeout(function () {
+        console.log('Async task is complete');
+        resolve();
+    }, 1000)
+})
+/* consume (consumption of promise) the Promises */
+// resolve ka connection hai seedha .then ke saath
+promisesone.then(function () {
+    console.log("Promise Consumed");
+})
+
+
+new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        console.log("Async task 2");
+        resolve()
+    }, 1000)
+
+}).then(function () {
+    console.log("Async 2 resolved");
+})
+
+
+const promiseThree = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve({ username: "Chai", email: "chai@example.com" })
+    }, 1000)
+})
+
+promiseThree.then(function (user) {
+    console.log(user);
+})
+
+
+
+const promiseFour = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        let error = true
+        if (!error) {
+            resolve({ username: "Akhilesh", password: "12321" })
+        } else {
+            reject('ERROR: Something went wrong')
+        }
+    }, 1000)
+})
+
+promiseFour.then((user) => {
+    console.log(user);
+    return user.username
+}).then((username) => {
+    console.log(username);
+}).catch(function (error) {
+    console.log(error);
+}).finally(() => console.log("The promise is either resolved or rejected"))
