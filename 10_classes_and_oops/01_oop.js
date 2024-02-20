463480014f83a0566496ce3f0f92d0d21143af25
akhilkumarbst014 <131418856+akhilkumarbst014@users.noreply.github.com>
2024-02-20 03:18:34 +0000

    oops

---
+++ 10_classes_and_oops/01_oop.js
@@ -0,0 +1,35 @@
+const user = {
+    // It is literaly an Object
+    username: "Akhil",
+    loginCount: 8,
+    signedIn: true,
+    getUserDetails:function (){
+        //console.log("Get User Detail from database");
+        //console.log(`Username: ${this.username}`);
+        console.log(this);
+    } 
+}
+
+//console.log(user.username)
+//console.log(user.getUserDetails());
+// console.log(this);
+
+
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+
+    }
+
+    return this
+}
+
+const userOne = new User("hitesh", 12, true)
+const userTwo = new User("ChaiAurCode", 11, false)
+console.log(userOne.constructor);
+//console.log(userTwo);
